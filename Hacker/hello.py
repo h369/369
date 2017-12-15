0bb6d741d3eeaf2bdd6faa200a079d9fde82ee36
h369 <h369@users.noreply.github.com>
2017-12-15 11:17:48 +0800

    第一次提交
    开始我得GitHub之旅

---
+++ Hacker/hello.py
@@ -0,0 +1,7 @@
+periodic_table = ['H','He','Li','Be','B','C','N','O','F','Ne']
+print(periodic_table[0])
+print(periodic_table[-2])
+print(periodic_table[0:3])
+print(periodic_table[-10:-7])
+print(periodic_table[-10:])
+print(periodic_table[:9])
